57c0c3a734a292cd45e7848458cd070897ac8625
Ree02 <ri.02kh.aax@gmail.com>
2022-08-28 14:32:56 +0900

    タスク表示形式変更

--- components/ShowTaskListCard.vue
+++ components/ShowTaskListCard.vue
@@ -1,21 +1,40 @@
 <template>
   <div>
-    <v-list class="margin-top">
-      <v-list-item v-for="(task, index) in tasks" :key="task.id">
-        <p class="list-index">{{ index + 1 }}</p>
-
-        <v-list-item-content>
-          <v-list-item-title v-text="task.title"></v-list-item-title>
-          <v-list-item-subtitle v-text="task.date"></v-list-item-subtitle>
-        </v-list-item-content>
-
-        <v-list-item-action>
-          <v-btn icon>
-            <v-icon color="grey lighten-1">mdi-information</v-icon>
-          </v-btn>
-        </v-list-item-action>
-      </v-list-item>
-    </v-list>
+    <v-container>
+      <div class="filter">
+        <v-row align="center" class="mb-6">
+          <v-spacer></v-spacer>
+          <v-col v-for="task in tasks" :key="task.id" class="pa-2" cols="auto">
+            <v-btn text elevation="0">{{ state }}</v-btn>
+          </v-col>
+          <v-spacer></v-spacer>
+        </v-row>
+      </div>
+    </v-container>
+    <v-container>
+      <v-simple-table fixed-header height="400px">
+        <template>
+          <thead>
+            <tr>
+              <th class="text-left">タスク名</th>
+              <th class="text-left">期限</th>
+              <th class="text-left">状態</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="task in tasks" :key="task.id">
+              <td>{{ task.title }}</td>
+              <td>{{ task.deadlineDate }}</td>
+              <td>
+                <v-btn outlined>{{ taskStatus(task.status) }}</v-btn>
+              </td>
+              <td><v-btn outlined color="error">編集</v-btn></td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </v-container>
   </div>
 </template>
 
@@ -24,24 +43,39 @@ import { defineComponent } from "@nuxtjs/composition-api";
 
 export default defineComponent({
   setup() {
+    const statusList = ["未完了", "完了", "期限切れ"];
+
     const tasks = [
       {
         id: 1,
-        color: "blue",
-        icon: "mdi-clipboard-text",
-        date: "Jan 20, 2014",
-        title: "Vacation itinerary",
+        title: "書類提出",
+        created: "2022/8/20 15:00",
+        deadlineDate: "2022/8/20 15:00",
+        status: 0,
       },
       {
         id: 2,
-        color: "amber",
-        icon: "mdi-gesture-tap-button",
-        date: "Jan 10, 2014",
-        title: "Kitchen remodel",
+        title: "買い物",
+        created: "2022/8/20 15:00",
+        deadlineDate: "2022/8/20 23:00",
+        status: 1,
+      },
+      {
+        id: 3,
+        title: "お風呂掃除",
+        created: "2022/8/20 15:00",
+        deadlineDate: "2022/8/15 09:00",
+        status: 2,
       },
     ];
+
+    const taskStatus = (statusId: number) => {
+      return statusList[statusId];
+    };
     return {
+      statusList,
       tasks,
+      taskStatus,
     };
   },
 });
